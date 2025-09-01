a2fe28425e23f7c04613efc6d89cfa8738bdbc01
rajib hossen <mhossen6311@gmail.com>
2025-09-01 13:09:30 +0600

    Project setup and Navbar update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+  return (
+    <div className="bg-primary w-[1440px] mx-auto h-[700px]">
+       <Navbar className=""/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import logo from '../assets/logo.png'
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="relative z-20">
+      <div className="w-[1280px] py-3 mx-auto md:flex">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <a href="#">
+            <img
+              className="w-auto h-6 sm:h-7"
+              src={logo}
+              alt="Logo"
+            />
+          </a>
+
+          {/* Mobile menu button */}
+          <div className="flex lg:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none"
+              aria-label="toggle menu"
+            >
+              {!isOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 8h16M4 16h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+
+        {/* Menu */}
+        <div
+          className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out md:mt-0 md:p-0 md:top-0 md:relative md:opacity-100 md:translate-x-0 md:flex md:items-center md:justify-between
+          ${isOpen ? "translate-x-0 opacity-100" : "opacity-0 -translate-x-full md:opacity-100"}`}
+        >
+          <div className="flex flex-col px-2 -mx-4 md:flex-row md:mx-10 md:py-0">
+            <a
+              href="#"
+              className="px-2.5 py-2 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="px-2.5 py-2 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="px-2.5 py-2  transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2"
+            >
+              Contact
+            </a>
+          </div>
+
+          <div className="flex gap-5">
+            <button className="text-white">Log in</button>
+            <button className="text-white bg-secondary py-2 px-2 rounded-lg">Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
