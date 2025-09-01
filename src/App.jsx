c37e694de28dd2d9c45e8b52f5eb1638780abc33
rajib hossen <mhossen6311@gmail.com>
2025-09-01 22:26:02 +0600

    mobile view card update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="bg-primary md:w-[1440px] mx-auto h-[800px] relative">
+    <div className="bg-primary md:w-[1440px] mx-auto md:h-[800px] h-[700px] relative">
        <Navbar/>
        <Cards/>
     </div>
---
+++ src/components/CardsMobile.jsx
@@ -0,0 +1,24 @@
+import card1 from '../assets/card1.png'
+import card2 from '../assets/card2.png'
+import card3 from '../assets/card3.png'
+import card4 from '../assets/card4.png'
+import card5 from '../assets/card5.png'
+
+
+const CardsMobile = () => {
+    return (
+        <div className="md:hidden">
+            <div className=''>
+            <img className='w-[315px] -left-20 absolute z-30 bottom-1' src={card1} alt="" />
+            <img className='w-[300px] -left-3 absolute z-35 bottom-1' src={card2} alt="" />
+            <img className='w-[340px] absolute z-40 pl-35 bottom-1' src={card3}
+             alt="" />
+            <img className='w-[2050px] left-28 absolute z-45 pl-20 bottom-1' src={card4} 
+            alt="" />
+            <img className='w-[1050px] left-30 absolute z-50 pl-10 bottom-1' src={card5} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default CardsMobile;
