8233eac23d3da75db31ad5679bc25295524798b0
rajib hossen <mhossen6311@gmail.com>
2025-09-02 01:28:54 +0600

    cta section desktop version update

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,18 +1,28 @@
-
+import cta_image from '../assets/cta_image.png'
 
 const Cta = () => {
-    return (
-        <div className="container mx-auto px-20 my-12">
-            <div className="h-[400px] bg-primary rounded-lg">
-                <div>
-                    <h1></h1>
-                </div>
-                <div>
-                    <img src="" alt="" />
-                </div>
-            </div>
+  return (
+    <div className="container mx-auto px-4 md:px-20 my-12">
+      <div className="rounded-lg flex flex-col md:flex-row items-center justify-between overflow-hidden">
+        {/* Text Section */}
+        <div className="w-full md:w-7/12 bg-primary h-[350px] flex flex-col items-center justify-center text-center md:text-left p-6 md:p-10">
+          <h1 className="text-2xl md:text-3xl font-semibold text-white">Give us a shot</h1>
+          <p className="text-white mt-3">
+            Join over 4,000+ startups already growing with Untitled.
+          </p>
         </div>
-    );
-};
 
-export default Cta;
+        {/* Image Section */}
+        <div className="w-full md:w-5/12">
+          <img
+            className="h-[250px] md:h-[350px] w-full object-cover"
+            src={cta_image}
+            alt="CTA Banner"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cta
--- src/components/SocialProof.jsx
+++ src/components/SocialProof.jsx
@@ -6,19 +6,22 @@ import s5 from '../assets/s5.png'
 import s6 from '../assets/s6.png'
 
 const SocialProof = () => {
-    return (
-        <div className='mt-12'>
-            <p className="text-[#475467] text-center">Join 4,000+ companies already growing</p>
-            <div className='grid grid-cols-2 md:flex items-center justify-center md:gap-12 mt-8'>
-                <img className='w-30 ' src={s1} alt="" />
-                <img className='w-30 ' src={s2} alt="" />
-                <img className='w-30 ' src={s3} alt="" />
-                <img className='w-30 ' src={s4} alt="" />
-                <img className='w-30 ' src={s5} alt="" />
-                <img className='w-30 ' src={s6} alt="" />
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="mt-12">
+      <p className="text-[#475467] text-center">
+        Join 4,000+ companies already growing
+      </p>
 
-export default SocialProof;
+      <div className="grid grid-cols-2 justify-items-center gap-6 md:flex md:items-center md:justify-center md:gap-12 mt-8">
+        <img className="w-28 md:w-30" src={s1} alt="logo 1" />
+        <img className="w-28 md:w-30" src={s2} alt="logo 2" />
+        <img className="w-28 md:w-30" src={s3} alt="logo 3" />
+        <img className="w-28 md:w-30" src={s4} alt="logo 4" />
+        <img className="w-28 md:w-30" src={s5} alt="logo 5" />
+        <img className="w-28 md:w-30" src={s6} alt="logo 6" />
+      </div>
+    </div>
+  )
+}
+
+export default SocialProof
