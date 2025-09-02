f1a791eb477eee2a7165268af9d5030e84e0e701
rajib hossen <mhossen6311@gmail.com>
2025-09-02 13:27:47 +0600

    Update All

--- src/components/CardsMobile.jsx
+++ src/components/CardsMobile.jsx
@@ -13,9 +13,9 @@ const CardsMobile = () => {
             <img className='w-[300px] -left-3 absolute z-35 bottom-0' src={card2} alt="" />
             <img className='w-[340px] absolute z-40 pl-35 bottom-0' src={card3}
              alt="" />
-            <img className='w-[2040px] left-26 absolute z-45 pl-21 bottom-0 ' src={card4} 
+            <img className='w-[1040px] left-26 absolute z-40 pl-20 bottom-0 ' src={card4} 
             alt="" />
-            <img className='w-[1050px] left-30 absolute z-50 pl-20 bottom-0 ' src={card5} alt="" />
+            <img className='w-[1050px] left-38 absolute z-50 pl-20 bottom-0 ' src={card5} alt="" />
             </div>
         </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,13 +23,13 @@ const Navbar = () => {
             <button
               onClick={() => setIsOpen(!isOpen)}
               type="button"
-              className="text-gray-500 dark:text-gray-200 focus:outline-none"
+              className="text-white dark:text-gray-200 focus:outline-none"
               aria-label="toggle menu"
             >
               {!isOpen ? (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
+                  className="w-6 h-6 text-white"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -44,7 +44,7 @@ const Navbar = () => {
               ) : (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
+                  className="w-6 h-6 text-white"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
